0181926aea14b09a9ab00772574deca4e762e131
Ben Petrosky <benpetrosky@hotmail.com>
2017-06-09 12:36:21 -0700

    displays number of caretakers in animal bio list

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -12,7 +12,7 @@ import { Animal } from './animal.model'
   </select>
 
   <ul>
-    <li *ngFor="let currentAnimal of childAnimalList | matureness:filterByMatureness">{{currentAnimal.name}} is a {{currentAnimal.age}} year old {{currentAnimal.sex}} {{currentAnimal.species}} that enjoys {{currentAnimal.likes}}, and does not like {{currentAnimal.dislikes}}. {{currentAnimal.name}} eats {{currentAnimal.diet}}. You can find {{currentAnimal.name}} in the {{currentAnimal.location}} exhibit.<button (click)="editButtonHasbeenClicked(currentAnimal)">
+    <li *ngFor="let currentAnimal of childAnimalList | matureness:filterByMatureness">{{currentAnimal.name}} is a {{currentAnimal.age}} year old {{currentAnimal.sex}} {{currentAnimal.species}} that enjoys {{currentAnimal.likes}}, and does not like {{currentAnimal.dislikes}}. {{currentAnimal.name}} eats {{currentAnimal.diet}}. You can find {{currentAnimal.name}} in the {{currentAnimal.location}} exhibit.  If you have any questions ask any of the {{currentAnimal.caretakers}} caretakers assigned to the animal.<button (click)="editButtonHasbeenClicked(currentAnimal)">
     Edit</button>
     <hr>
     </li>
